8227c1f0a8e6649960cdbd8374502a5c638cc035
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-19 11:57:53 -0600

    added postgreSQL

--- knexfile.js
+++ knexfile.js
@@ -19,16 +19,8 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/shouts.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
@@ -37,3 +29,5 @@ module.exports = {
     },
   }
 };
+
+// postgres://yltlkwlxjrwyld:fd0b4d15f99519c8122b670e17348fb5d2f119eccdee6326bc0a8ceb27c05973@ec2-107-20-167-241.compute-1.amazonaws.com:5432/damk7apg0duaf8
